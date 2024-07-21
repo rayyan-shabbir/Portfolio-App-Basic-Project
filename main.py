3edb43f0aa6143f4da90c191f0bb4e4b7e20f54b
rayan-shabir <bitf20m535@pucit.edu.pk>
2024-07-21 08:57:46 +0500

    Add description on the new columns.

--- main.py
+++ main.py
@@ -5,12 +5,24 @@ import streamlit as st
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photoo.jpg", width=220)
+    st.image("images/photoo.jpg")
 
 with col2:
     st.title("Rayyan Shabbir")
     content = """
     I am a Software engineers, and interest in AI.
-    I have done my bachelors in IT from PUCIT, and below are all my projects.
-"""
+    I have done my bachelors in IT from PUCIT.
+    I am skilled in Python, Java, C, Cpp, ML and DL. 
+    I wanted to serve people and channge their lives with technology.
+
+    IT is future, and everyone must put efforts to come on that side where you will control technology.
+    
+    If you are from some other degree like business, marketinng, construction or any other, you can also switch to IT.
+    As, there are many resources available online, and the most popular sites are coursera, udemy, edureka, educative etc.
+
+    Explore these and take the first step.
+    """
     st.info(content)
+
+content2 = "Below you can find some of apps I have built in Python. Feel free to contact me!"
+st.write(content2)
